ed2bb3e6a0896ac9ebd3031c2458c3e15359951c
Alex Coady <alexandercoady@me.com>
2014-08-08 22:14:23 +0100

    Tidied from old repo

--- source/scripts/main.js
+++ source/scripts/main.js
@@ -22,56 +22,5 @@
 
 
 // start app
-var PageHandler       = require('./components/page-handler'),
-    PluginHandler     = require('./components/plugin-handler');
-
-var pluginHandler = new PluginHandler();
-pluginHandler.init();
-
-var pageHandler = PageHandler.getInstance();
-pageHandler.init( '#Chrome-main', {
-  pluginHandler: pluginHandler
-});
-
-
-
-(function(){
-
-  // Add device class
-  var body = document.body,
-      html = body.parentNode;
-
-  if( window.isDesktop ) html.addClass('is-desktop');
-  if( window.isTablet ) html.addClass('is-tablet');
-  if( window.isMobile ) html.addClass('is-mobile');
-  if( window.isIE ) html.addClass('is-ie');
-  if( window.hasMouseMove ) html.addClass('mousemove');
-
-  if( !window.isDesktop ) return;
-  // ----------------------------------
-
-  // Scroll optimization
-  // http://www.thecssninja.com/javascript/pointer-events-60fps
-
-  var cover = document.createElement('div'),
-      isScrollCovered = false,
-      timer;
-
-  cover.setAttribute('class', 'ScrollCover');
-
-  // window.addEventListener('scroll', function() {
-  //   if( timer ) clearTimeout( timer );
-
-  //   if( !isScrollCovered ){
-  //     isScrollCovered = true;
-  //     body.appendChild(cover);
-  //   }
-
-  //   timer = setTimeout(function(){
-  //     if( isScrollCovered ){
-  //       isScrollCovered = false;
-  //       body.removeChild( cover );
-  //     }
-  //   }, 250);
-  // }, false);
-})();
+// var PageHandler       = require('./components/page-handler'),
+//     PluginHandler     = require('./components/plugin-handler');
